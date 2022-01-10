21e577c0b2d8770ec62b61e810582c4cb2771f4a
KKhushnuma <96369685+KKhushnuma@users.noreply.github.com>
2022-01-10 22:35:42 +0530

    Add files via upload

---
+++ stringequalcheck.java
@@ -0,0 +1,10 @@
+package stringequalcheck;
+
+public class stringequalcheck {
+
+  public static void main(String[] args) {
+   String str1="English";
+   String str2="ENGLISH";
+   System.out.println(str1.equals(str2));
+  }
+}
