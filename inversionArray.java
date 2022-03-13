6de9c530270c3012aebeeb1211b0bb967fcd6911
KKhushnuma <96369685+KKhushnuma@users.noreply.github.com>
2022-03-13 20:41:36 +0530

    Add files via upload

---
+++ inversionArray.java
@@ -0,0 +1,31 @@
+package inversionArray;
+
+public class inversionArray {
+
+   static int arr[] = new int[] { 2, 20, 9, 8, 5,3};
+ 
+
+    static int getInvCount(int n)
+    {
+    int inv_count = 0;
+
+        for (int i = 0; i < n - 1; i++)
+
+            for (int j = i + 1; j < n; j++)
+
+                if (arr[i] > arr[j])
+
+                    inv_count++;
+ 
+
+        return inv_count;
+
+    }
+
+
+    public static void main(String[] args)
+
+    {
+    System.out.println("Number of inversions are "   + getInvCount(arr.length));
+  }
+}
